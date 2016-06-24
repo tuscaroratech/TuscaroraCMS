d4a65bd139c47cc365e1097a99e7488b9863b979
Jeremy <jeremy.r.foltz@gmail.com>
2016-06-24 13:41:15 -0400

    Adding Docker Support

--- src/TuscaroraCMS/Program.cs
+++ src/TuscaroraCMS/Program.cs
@@ -12,6 +12,7 @@ namespace TuscaroraCMS
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? String.Empty)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
